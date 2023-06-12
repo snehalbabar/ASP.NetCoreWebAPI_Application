a041ff679ef3162cdc615b9688c1d0242f34f3b3
snehalbabar <h728e344@wichita.edu>
2023-06-12 13:14:26 -0500

    uploading structure of web api project

---
+++ Data/CntactsAPIDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ContactAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactAPI.Data
+{
+    public class CntactsAPIDbContext : DbContext
+    {
+        public CntactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts{ get; set; }
+
+    }
+}
+
---
+++ Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ContactAPI.Models
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public long Phone { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
+
